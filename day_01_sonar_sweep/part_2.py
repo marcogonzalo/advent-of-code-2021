034ee7010d860ee107e9c101045493090075d819
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2021-12-08 20:23:21 +0000

    Day 1: Part 2

--- day_01_sonar_sweep/part_2.py
+++ day_01_sonar_sweep/part_2.py
@@ -5,15 +5,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.input_reader import *
 
 
-def count_measurement_increments(measurements):
+def count_measurement_increments(measurements, window_size=3):
     measuremnts_count = len(measurements)
-    if measuremnts_count <= 1:
+    if measuremnts_count <= window_size:
         return 0 
-
+    
     counter = 0
-    for m in range(1,measuremnts_count):
-        if measurements[m] > measurements[m-1]:
+    previous_sum = sum(measurements[0:window_size])
+    for m in range(window_size, measuremnts_count):
+        current_sum = previous_sum - measurements[m - window_size] + measurements[m]
+        if current_sum > previous_sum:
             counter = counter + 1 
+        previous_sum = current_sum
     return counter
 
 def main():
