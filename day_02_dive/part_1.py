0b2de779294646ff87bd1aeef1f0d7e207595268
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2021-12-08 20:50:56 +0000

    Day 2: Part 1

---
+++ day_02_dive/part_1.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.input_reader import *
+
+
+def calc_last_position(course):
+    horizontal_pos = 0
+    depth = 0
+    for c in course:
+        command, value = c.split(' ')
+        units = int(value)
+        if command == 'up':
+            depth -= units 
+        elif command == 'down':
+            depth += units 
+        elif command == 'forward':
+            horizontal_pos += units 
+        else:
+            continue
+    return horizontal_pos, depth
+
+
+def multiply_last_position(course):
+    horizontal_pos, depth = calc_last_position(course)
+    return horizontal_pos * depth
+
+
+def main():
+    file_path = os.path.dirname(__file__)  + '/input.txt'
+    values = get_input_array(filename=file_path)
+    position_product = multiply_last_position(values)
+    print("Last position value: {}".format(position_product))
+
+
+main()
